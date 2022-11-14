21a00b27c4f07825836e79cc68c4950250d80c7d
李小钧 <tingjunl@umich.edu>
2022-11-13 20:07:12 -0500

    Mobile view

---
+++ js/utils.js
@@ -0,0 +1,13 @@
+// Function to increase image size
+function enlargeImg(img) {
+//   // Set image size to 1.5 times original
+//   img.style.transform = "scale(1.5)";
+//   // Animation effect
+//   img.style.transition = "transform 0.25s ease";
+}
+// Function to reset image size
+function resetImg(img) {
+//   // Set image size to original
+//   img.style.transform = "scale(1)";
+//   img.style.transition = "transform 0.25s ease";
+}
